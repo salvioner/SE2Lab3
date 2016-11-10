82480101e341bad52c2aa11a85eab59b147d5a2f
MattiaSalnitri <mattia@Mattias-MacBook-Pro-3.local>
2016-11-10 10:32:26 +0100

    upload all exercises

---
+++ 3/index.js
@@ -0,0 +1,32 @@
+//Libraries (default)
+var http = require('http');
+
+//for templates
+var bind = require('bind');
+ 
+//create a server
+var server = http.createServer(function (req, res) 
+{
+ 
+    //bind to template
+	bind.toFile('tpl/home.tpl', 
+	{
+        //set up parameters
+        address: 'via Roma',
+        city: 'Milano',
+        resident : true
+    }, 
+    function(data) 
+    {
+        //write response
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+    
+});
+ 
+//listen in a specific port
+server.listen(1337, '127.0.0.1');
+ 
+//check status
+console.log('Server running at http://127.0.0.1:1337/');
